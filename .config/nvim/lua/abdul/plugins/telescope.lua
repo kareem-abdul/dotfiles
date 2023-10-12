626bc7be9e0f32e4030da7970f2290cca96ca4f6
kareem-abdul <abdul.k@flycatchtech.com>
2023-10-12 15:55:16 +0530

    feat(nvim): added telescope fzf extension and web-icons

--- .config/nvim/lua/abdul/plugins/telescope.lua
+++ .config/nvim/lua/abdul/plugins/telescope.lua
@@ -1,6 +1,12 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.3',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.3',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        'kareem-abdul/harpoon',
+        'nvim-tree/nvim-web-devicons',
+    },
     config = function()
         local mappings = require("abdul.core.remap").telescope_keymaps();
         require("telescope").setup({
@@ -8,8 +14,8 @@ return {
                 mappings = mappings
             }
         })
+        require('telescope').load_extension('fzf')
         require("telescope").load_extension('harpoon')
-
     end
 
 }
