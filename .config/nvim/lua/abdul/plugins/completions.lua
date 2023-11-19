d7a8ff9a3aa780359e6d09a2584bff5ed3dce492
kareem-abdul <abdul.k@flycatchtech.com>
2023-11-20 00:39:18 +0530

    feat(nvim): added friendly-snippets and disable auto select and insert from completions

--- .config/nvim/lua/abdul/plugins/completions.lua
+++ .config/nvim/lua/abdul/plugins/completions.lua
@@ -1,38 +1,38 @@
 return {
     'hrsh7th/nvim-cmp',
-    event = { "InsertEnter"},
+    event = { "InsertEnter" },
     dependencies = {
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-nvim-lua',
         'hrsh7th/cmp-cmdline',
         'hrsh7th/cmp-nvim-lsp',
-        "L3MON4D3/LuaSnip",
+        { "L3MON4D3/LuaSnip", dependencies = { "rafamadriz/friendly-snippets" } },
         'saadparwaiz1/cmp_luasnip',
     },
     config = function()
         local cmp = require('cmp');
         local luasnip = require('luasnip');
-       cmp.setup({
-           experimental = {
-               ghost_text = true,
-           },
-           snippet = {
+        cmp.setup({
+            experimental = {
+                ghost_text = true,
+            },
+            snippet = {
                 expand = function(args)
                     luasnip.lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert(require('abdul.core.remap').nvim_cmp_completions()),
             completion = {
-                completeopt = 'menu,menuone,preview,noinsert',
+                completeopt = 'menu,menuone,preview,noinsert,noselect',
             },
             sources = cmp.config.sources({
                 { name = 'nvim_lsp', priority = 1000 },
-                { name = 'luasnip', priority = 750 },
+                { name = 'luasnip',  priority = 750 },
 
                 { name = 'nvim_lua', priority = 500 },
-                { name = 'path', priority = 250 },
-                { name = 'buffer', priority = 150 },
+                { name = 'path',     priority = 250 },
+                { name = 'buffer',   priority = 150 },
             }),
 
         });
