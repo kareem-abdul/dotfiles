579e3083fdf4dd46ae120fbbe2b46e24f0cfe492
kareem-abdul <abdul.k@flycatchtech.com>
2023-11-20 00:52:04 +0530

    feat(nvim): added custom handler for nvim-ufo
    Also fixed issues with nvim-ufo

--- .config/nvim/lua/abdul/plugins/nvim-ufo.lua
+++ .config/nvim/lua/abdul/plugins/nvim-ufo.lua
@@ -1,10 +1,37 @@
+local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, { suffix, 'MoreMsg' })
+    return newVirtText
+end
 -- adds code folding
 return {
     "kevinhwang91/nvim-ufo",
     dependencies = { 'kevinhwang91/promise-async' },
-    event = { "BufReadPre", "BufNewFile" },
+    event = { "BufRead", "BufNewFile" },
     config = function()
-        vim.o.foldcolumn = '1' -- '0' is not bad
+        vim.o.foldcolumn = '0' -- '0' is not bad
         vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
@@ -14,8 +41,9 @@ return {
 
         require('ufo').setup({
             provider_selector = function(bufnr, filetype, buftype)
-                return {'lsp', 'treesitter', 'indent' }
-            end
+                return { 'treesitter', 'indent' }
+            end,
+            fold_virt_text_handler = handler
         })
     end
 }
