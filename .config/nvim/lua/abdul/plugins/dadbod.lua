c9a9f99d2d367bc54da6b8871557d8feaf6ac046
kareem-abdul <abdul.k@flycatchtech.com>
2023-11-20 23:06:44 +0530

    feat(nvim): added dadbod plugin

---
+++ .config/nvim/lua/abdul/plugins/dadbod.lua
@@ -0,0 +1,15 @@
+-- a plugin to manage databases from nvim
+-- return {
+--     'kristijanhusak/vim-dadbod-ui',
+--     lazy = true,
+--     enabled = false, -- does not work with docker
+--     dependencies = {
+--         'tpope/vim-dadbod'
+--     },
+--     cmd = {
+--         'DBUI',
+--         'DBUIToggle',
+--         'DBUIAddConnection',
+--         'DBUIFindBuffer',
+--     },
+-- };
