a34cc0bbbd33859af637ee89d0e0c0ad3ea87473
kareem-abdul <abdul.k@flycatchtech.com>
2023-11-21 00:41:05 +0530

    refactor(setup): updated install script

--- setup.sh
+++ setup.sh
@@ -18,21 +18,22 @@ function isInstalled() {
 
 # scripts
 if ! isInstalled "scripts"; then
-    echo "setting up scripts"
-    for script in $(pwd)/.local/bin/*; do
-        echo "linking $script"
-        ln -s $script ~/.local/bin/$(basename $script)
-    done
+    echo "setting up scripts to ~/.local/bin"
+    ln -s $PWD/scripts ~/.local/bin/scripts
     echo "scripts" >> installed
+    echo "make sure that the path $HOME/.local/bin/scripts is in your PATH variable"
 fi
 
 # tmux
 if ! isInstalled "tmux"; then
     echo "Setting up tmux config"
     if [[ -f "$HOME/.tmux.conf" ]]; then
-        mv ~/.tmux.conf ~/.dotfiles.bak/.tmux.conf.$(date "+%H:%M:%S:%N")
+        mv ~/.tmux.conf ~/.dotfiles.bak/.tmux.conf.$(date "+%H:%M:%S:%N").bak
     fi
-    ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+    if [[ -d "$HOME/.config/tmux" ]]; then
+	mv ~/.config/tmux ~/.dotfiles.bak/tmux.$(date "+%H:%M:%S:%N").bak
+    fi
+    ln -s $PWD/config/tmux ~/.config/tmux
     echo "tmux" >> installed
 fi
 
@@ -40,8 +41,8 @@ fi
 if ! isInstalled "nvim"; then 
     echo "setting up nvim config"
     if [[ -d "$HOME/.config/.nvim" ]]; then
-        mv ~/.config/nvim ~/.dotfiles.bak/.config/nvim.$(date "+H:%M:%S:%N")
+        mv ~/.config/nvim ~/.dotfiles.bak/.config/nvim.$(date "+H:%M:%S:%N").bak
     fi
-    ln -s $(pwd)/.config/nvim ~/.config/nvim
+    ln -s $PWD/config/nvim ~/.config/nvim
     echo "nvim" >> installed
 fi
