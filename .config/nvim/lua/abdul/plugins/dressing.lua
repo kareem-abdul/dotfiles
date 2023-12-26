b26ec0a09e70545ab5415cdea6b5d69cbccc2ccf
kareem-abdul <abdul.k@flycatchtech.com>
2023-12-27 01:42:46 +0530

    feat(nvim): updates dressing config

--- .config/nvim/lua/abdul/plugins/dressing.lua
+++ .config/nvim/lua/abdul/plugins/dressing.lua
@@ -1,43 +1,53 @@
 return {
     'stevearc/dressing.nvim',
     event = "VeryLazy",
-    opts = {
-        input = {
-            enabled = true,
-            default_prompt = "Input:",
-            title_pos = "left",
-            insert_only = true,
-            start_in_insert = true,
-            border = "rounded",
-            relative = "editor",
-            win_options = {
-                winblend = 10,
-                wrap = false,
-                list = true,
-                listchars = "precedes:…,extends:…",
-                sidescrolloff = 0,
-            },
+    config = function()
+        require('dressing')
+            .setup({
+                input = {
+                    enabled = true,
+                    default_prompt = "Input:",
+                    title_pos = "left",
+                    insert_only = true,
+                    start_in_insert = true,
+                    border = "rounded",
+                    relative = "editor",
+                    win_options = {
+                        winblend = 10,
+                        wrap = false,
+                        list = true,
+                        listchars = "precedes:…,extends:…",
+                        sidescrolloff = 0,
+                    },
 
-            mappings = {
-                n = {
-                    ["<Esc>"] = "Close",
-                    ["<CR>"] = "Confirm",
-                },
-                i = {
-                    ["<C-c>"] = "Close",
-                    ["<CR>"] = "Confirm",
-                    ["<C-k>"] = "HistoryPrev",
-                    ["<C-j>"] = "HistoryNext",
-                },
-            },
+                    mappings = {
+                        n = {
+                            ["<Esc>"] = "Close",
+                            ["<CR>"] = "Confirm",
+                        },
+                        i = {
+                            ["<C-c>"] = "Close",
+                            ["<CR>"] = "Confirm",
+                            ["<C-k>"] = "HistoryPrev",
+                            ["<C-j>"] = "HistoryNext",
+                        },
+                    },
 
-        },
-        select = {
-            enabled = true,
-            backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
-            trim_prompt = true,
-        },
-    },
+                },
+                select = {
+                    enabled = true,
+                    backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
+                    trim_prompt = true,
+                    telescope = require 'telescope.themes'.get_dropdown({
+                        layout_config = {
+                            width = function(_, max_columns)
+                                local percentage = 0.5
+                                return math.floor(percentage * max_columns)
+                            end,
+                        },
+                    })
+                },
+            })
+    end
 
 }
-
