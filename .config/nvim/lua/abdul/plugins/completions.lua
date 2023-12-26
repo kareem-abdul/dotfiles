807ce21ee1d99910f2fa1b703d03ed2a48c11e96
kareem-abdul <abdul.k@flycatchtech.com>
2023-12-27 00:55:30 +0530

    refactor(nvim): changes the order of completions

--- .config/nvim/lua/abdul/plugins/completions.lua
+++ .config/nvim/lua/abdul/plugins/completions.lua
@@ -7,12 +7,15 @@ return {
         'hrsh7th/cmp-nvim-lua',
         'hrsh7th/cmp-cmdline',
         'hrsh7th/cmp-nvim-lsp',
-        { "L3MON4D3/LuaSnip", dependencies = { "rafamadriz/friendly-snippets" } },
+        "L3MON4D3/LuaSnip",
         'saadparwaiz1/cmp_luasnip',
+        "rafamadriz/friendly-snippets"
     },
     config = function()
         local cmp = require('cmp');
         local luasnip = require('luasnip');
+
+        require("luasnip.loaders.from_vscode").lazy_load()
         cmp.setup({
             experimental = {
                 ghost_text = true,
@@ -24,17 +27,28 @@ return {
             },
             mapping = cmp.mapping.preset.insert(require('abdul.core.remap').nvim_cmp_completions()),
             completion = {
-                completeopt = 'menu,menuone,preview,noinsert,noselect',
+                completeopt = 'menu,menuone,preview,noselect',
             },
             sources = cmp.config.sources({
-                { name = 'nvim_lsp', priority = 1000 },
-                { name = 'luasnip',  priority = 750 },
+                { name = 'buffer' },
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
 
-                { name = 'nvim_lua', priority = 500 },
-                { name = 'path',     priority = 250 },
-                { name = 'buffer',   priority = 150 },
+                { name = 'nvim_lua' },
+                { name = 'path' },
             }),
-
+            formatting = {
+                format = function(entry, vim_item)
+                    vim_item.menu = ({
+                        nvim_lsp = "[LSP]",
+                        luasnip = "[LuaSnip]",
+                        nvim_lua = "[Lua]",
+                        path = "[path]",
+                        buffer = "[Buffer]",
+                    })[entry.source.name]
+                    return vim_item
+                end
+            },
         });
         cmp.setup.cmdline(':', {
             mapping = cmp.mapping.preset.cmdline(),
