b18f828e5e5060d7dd325bd749aba40288314aa0
kareem-abdul <abdul.k@flycatchtech.com>
2023-12-30 17:40:58 +0530

    feat(mason): added auto installer for tools

--- .config/nvim/lua/abdul/plugins/mason.lua
+++ .config/nvim/lua/abdul/plugins/mason.lua
@@ -1,7 +1,8 @@
-return    {
+return {
     "williamboman/mason.nvim",
     dependencies = {
-        'williamboman/mason-lspconfig.nvim'
+        'williamboman/mason-lspconfig.nvim',
+        'WhoIsSethDaniel/mason-tool-installer.nvim'
     },
     config = function()
         require('mason').setup({
@@ -17,6 +18,15 @@ return    {
                 "github:mason-org/mason-registry"
             }
         });
+
+        require('mason-tool-installer').setup({
+            ensure_installed = {
+                "yq",
+                "java-test",
+                "java-debug-adaptor"
+            },
+            run_on_start = true
+        });
         require('mason-lspconfig').setup({
             ensure_installed = {
                 "dockerls",
@@ -24,12 +34,11 @@ return    {
                 "jsonls",
                 "tsserver",
                 "lua_ls",
-                "jdtls", -- eclipse java lsp
-                "marksman", -- markdown lsp 
+                "jdtls",    -- eclipse java lsp
+                "marksman", -- markdown lsp
                 "spectral", -- openapi lsp server
             },
             automatic_installation = true,
         });
     end
 }
-
