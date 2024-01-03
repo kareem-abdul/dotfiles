9822d88de3fe3e1d513d281e8bc59d8f51f21f4d
kareem-abdul <abdul.k@flycatchtech.com>
2024-01-03 19:36:35 +0530

    feat(git): added vim fugitive

--- .config/nvim/lua/abdul/plugins/gitsigns.lua
+++ .config/nvim/lua/abdul/plugins/gitsigns.lua
@@ -1,5 +1,8 @@
 return {
     "lewis6991/gitsigns.nvim",
+    dependencies = {
+        "tpope/vim-fugitive",
+    },
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("gitsigns").setup({
