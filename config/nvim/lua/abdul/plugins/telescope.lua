231e21bc571a72876d2288409af052dfcdb1f95d
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-01-07 21:24:23 +0530

    feat(nvim): adds custom path format for telescop

--- config/nvim/lua/abdul/plugins/telescope.lua
+++ config/nvim/lua/abdul/plugins/telescope.lua
@@ -18,6 +18,23 @@ return {
             return f_path(entry, validate, escape)
         end
 
+        local function filenameFirst(_, path)
+            local tail = vim.fs.basename(path)
+            local parent = vim.fs.dirname(path)
+            if parent == "." then return tail end
+            return string.format("%s\t-\t%s", tail, parent)
+        end
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "TelescopeResults",
+            callback = function(ctx)
+                vim.api.nvim_buf_call(ctx.buf, function()
+                    vim.fn.matchadd("TelescopeParent", "\t-\t.*$")
+                    vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+                end)
+            end,
+        })
+
         require("telescope").setup({
             defaults = {
                 vimgrep_arguments = {
@@ -32,7 +49,7 @@ return {
                 },
                 file_ignore_patterns = { ".git", "node_modules" },
                 mappings = require("abdul.core.remap").telescope_keymaps(),
-                path_diplay = { shorten = 3 },
+                path_display =  filenameFirst,
                 sorting_strategy = "ascending",
                 layout_config = {
                     horizontal = {
