20893764f42c50439a97cb27fd7251bcd54a5c39
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-01-24 20:22:10 +0530

    feat(nvim): adds custom layout for nvim-dap-ui

--- config/nvim/lua/abdul/plugins/nvim-dap.lua
+++ config/nvim/lua/abdul/plugins/nvim-dap.lua
@@ -1,3 +1,25 @@
+
+local dap_ui_layout = {
+                {
+                    elements = {
+                        { id = "watches",     size = 0.25 },
+                        { id = "breakpoints", size = 0.25 },
+                        { id = "scopes",      size = 0.25 },
+                        { id = "stacks",      size = 0.25 },
+                    },
+                    position = "left",
+                    size = 40
+                },
+                {
+                    elements = {
+                        { id = "console", size = 1 },
+                        -- { id = "repl", size = 0.5 }
+                    },
+                    position = "bottom",
+                    size = 10
+                }
+};
+
 return {
     "mfussenegger/nvim-dap",
     dependencies = {
@@ -5,7 +27,13 @@ return {
     },
     config = function()
         local dap, dapui = require("dap"), require("dapui")
-        dapui.setup()
+        dapui.setup({
+            layouts = dap_ui_layout,
+            controls = {
+                enabled = true,
+                element = "console"
+            }
+        })
         dap.listeners.after.event_initialized["dapui_config"] = function()
             dapui.open()
         end
