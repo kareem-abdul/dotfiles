0560e2203a9d2cb2c736539b7b3a736deae8d935
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-02-01 23:30:26 +0530

    feat(dap): loads dap config on startup

--- config/nvim/lua/abdul/plugins/nvim-dap.lua
+++ config/nvim/lua/abdul/plugins/nvim-dap.lua
@@ -1,23 +1,22 @@
-
 local dap_ui_layout = {
-                {
-                    elements = {
-                        { id = "watches",     size = 0.25 },
-                        { id = "breakpoints", size = 0.25 },
-                        { id = "scopes",      size = 0.25 },
-                        { id = "stacks",      size = 0.25 },
-                    },
-                    position = "left",
-                    size = 40
-                },
-                {
-                    elements = {
-                        { id = "console", size = 1 },
-                        -- { id = "repl", size = 0.5 }
-                    },
-                    position = "bottom",
-                    size = 10
-                }
+    {
+        elements = {
+            { id = "watches",     size = 0.25 },
+            { id = "breakpoints", size = 0.25 },
+            { id = "scopes",      size = 0.25 },
+            { id = "stacks",      size = 0.25 },
+        },
+        position = "left",
+        size = 40
+    },
+    {
+        elements = {
+            { id = "console", size = 1 },
+            -- { id = "repl", size = 0.5 }
+        },
+        position = "bottom",
+        size = 10
+    }
 };
 
 return {
@@ -45,5 +44,13 @@ return {
             dapui.close()
         end
 
+        local dap_configs = vim.api.nvim_get_runtime_file("lua/abdul/plugins/dap/*.lua", true)
+        for _, path in ipairs(dap_configs) do
+            local file = vim.fs.basename(path)
+            if not file or not vim.endswith(file, ".lua") then
+                break
+            end
+            print("abdul.plugins.dap." .. string.gsub(file, ".lua", ""))
+        end
     end
 }
