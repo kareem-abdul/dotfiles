47e2f2e5c2258c480b55ddbc15a0cc5a48d47394
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-02-01 23:07:06 +0530

    feat(dap): adds ts debugger

---
+++ config/nvim/lua/abdul/plugins/dap/typescript.lua
@@ -0,0 +1,35 @@
+local dap = require("dap")
+
+if not dap.adapters["pwa-node"] then
+    local debugger_path = vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter'
+    dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+            command = debugger_path .. "/js-debug-adapter",
+            args = { "${port}" }
+        }
+    }
+end
+
+local ts_loader = vim.fn.stdpath('data') .. '/mason/packages/ts-node/esm.mjs'
+dap.configurations.typescript = {
+    {
+        type = "pwa-node",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+        runtimeArgs = { '--loader=' .. ts_loader },
+        sourceMaps = true,
+        protocol = 'inspector',
+        outFiles = { "${workspaceFolder}/**/**/*", "!**/node_modules/**" },
+        skipFiles = { '<node_internals>/**', 'node_modules/**' },
+        resolveSourceMapLocations = {
+            "${workspaceFolder}/**",
+            "!**/node_modules/**",
+        },
+    },
+
+}
