db34bfc35b9fdc1b7da31d0370bb04f84f2f039e
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-02-16 20:34:06 +0530

    feat(nvim): makes telescop fzf ignore case

--- config/nvim/lua/abdul/plugins/telescope.lua
+++ config/nvim/lua/abdul/plugins/telescope.lua
@@ -69,6 +69,15 @@ return {
                         return true
                     end,
                 }
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,     -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "ignore_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
             }
         })
         telescope.load_extension('fzf')
