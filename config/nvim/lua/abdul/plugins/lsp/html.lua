e979979d32ea2971b09e0db911371cfd9da574b5
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-02-17 20:52:37 +0530

    feat(nvim): adds html lsp

---
+++ config/nvim/lua/abdul/plugins/lsp/html.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+local lspconfig = require('lspconfig')
+
+function M.setup(capabilities, on_attach)
+    capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    lspconfig.html.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "html" },
+        init_options = {
+            configurationSection = { "html", "css", "javascript" },
+            embeddedLanguages = {
+                css = true,
+                javascript = true
+            }
+        }
+
+    })
+end
+
+return M
