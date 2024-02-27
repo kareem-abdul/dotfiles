5c480347a7adc0b0a65add694339e844342b45ee
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-02-27 17:11:36 +0530

    feat(nvim): limitted tailwindcss lsp filetypes

---
+++ config/nvim/lua/abdul/plugins/lsp/tailwindcss.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+local lspconfig = require('lspconfig')
+
+function M.setup(capabilities, on_attach)
+    local default_config = require('lspconfig.server_configurations.tailwindcss').default_config;
+    lspconfig.tailwindcss.setup(
+        vim.tbl_deep_extend('force', default_config, {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            filetypes = {
+                "django-html",
+                "htmldjango",
+                "ejs",
+                'erb',
+                'eruby', -- vim ft
+                "gohtml",
+                "gohtmltmpl",
+                "html",
+                "html-eex",
+                "markdown",
+                "mdx",
+                "mustache",
+                "php",
+                "css",
+                "sass",
+                "scss",
+                "javascriptreact",
+                "typescriptreact",
+                "vue",
+                "svelte",
+                "templ"
+            },
+        })
+
+    )
+end
+
+return M
