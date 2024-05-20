b4af50632f06c820e8ce41154d23df7b258e4a06
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-05-20 16:21:23 +0530

    feat(nvim): adds venv manager plugin

---
+++ config/nvim/lua/abdul/plugins/venv-selector.lua
@@ -0,0 +1,25 @@
+-- manages activates venv on running neovim instances
+return {
+    "linux-cultist/venv-selector.nvim",
+    lazy = true,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+        require("venv-selector").setup({
+            settings = {
+                options = {
+                    debug = false,
+                    fd_binary_name = "find",
+                    enable_default_searches = false, -- requires fd command to be installed https://github.com/sharkdp/fd
+                },
+                search = {
+                    my_venv = {
+                        command = "$FD " .. vim.fn.getcwd() .. " -name python"
+                    }
+                },
+            }
+        })
+    end,
+    cmd = {
+        "VenvSelect"
+    },
+};
