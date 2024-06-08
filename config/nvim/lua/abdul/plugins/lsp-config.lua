2496ea8557af06b9e5153347bda6cdb124a4f29b
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-06-09 04:01:12 +0530

    feat(nvim): adds functionality to LspAttach as an autocmd
    The functionality include
     - only setup remap during attach
     - auto highlight on cursor hold and auto clear

--- config/nvim/lua/abdul/plugins/lsp-config.lua
+++ config/nvim/lua/abdul/plugins/lsp-config.lua
@@ -6,6 +6,45 @@ local function setup_signs()
     end
 end
 
+local function setup_autocommands()
+    local lsp_attach_group = vim.api.nvim_create_augroup("nvim-lsp-attach", { clear = true })
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = lsp_attach_group,
+        callback = function(event)
+            local bufnr = event.buf;
+            local client = vim.lsp.get_client_by_id(event.data.client_id)
+            require("abdul.core.remap").lsp_config_keymaps(client, bufnr)
+            if client and client.server_capabilities.documentHighlightProvider then
+                local highlight_augroup = vim.api.nvim_create_augroup("nvim-lsp-highlight-group", { clear = true })
+
+                vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                    buffer = bufnr,
+                    group = highlight_augroup,
+                    callback = vim.lsp.buf.document_highlight
+                })
+
+                vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                    buffer = bufnr,
+                    group = highlight_augroup,
+                    callback = vim.lsp.buf.clear_references
+                })
+
+                vim.api.nvim_create_autocmd("LspDetach", {
+                    buffer = bufnr,
+                    group = vim.api.nvim_create_augroup("nvim-lsp-dettach", { clear = true }),
+                    callback = function(event2)
+                        vim.lsp.buf.clear_references()
+                        vim.api.nvim_clear_autocmds({
+                            group = "nvim-lsp-highlight-group",
+                            buffer = event2.buf
+                        })
+                    end
+                })
+            end
+        end
+    })
+end
+
 return {
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -14,10 +53,10 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         local lspconfig = require('lspconfig');
-        local remap = require("abdul.core.remap");
         local utils = require("abdul.core.utils");
 
         setup_signs();
+        setup_autocommands()
 
         local capabilities = vim.tbl_deep_extend(
             "force",
