db3b910dd83aa804e9db1c80700af66be63829f3
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-07-09 21:28:05 +0530

    refactor(nvim): reverts git-worktree to main
    It seems the v2 branch is not stable as of now

--- config/nvim/lua/abdul/plugins/git-worktree.lua
+++ config/nvim/lua/abdul/plugins/git-worktree.lua
@@ -1,12 +1,13 @@
 return {
     "polarmutex/git-worktree.nvim",
-    branch="v2",
+    -- branch="v2",
     dependencies = { "nvim-lua/plenary.nvim" },
     enable = function()
         return vim.fs.find(".git", { upward = true, path = vim.fn.getcwd() })[1] ~= nil
     end,
     lazy = false,
     config = function()
+        -- vim.g.git_worktree_log_level = 'trace'
         local worktree = require("git-worktree")
         worktree.setup({
             update_on_change = true,
