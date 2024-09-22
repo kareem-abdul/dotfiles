a09e293d3d4dd7f8b8da4dcf87406f3ed519e43f
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-22 14:24:03 +0530

    fix(zsh): resolves invalid path in nvm plugin

--- config/zsh/plugins/nvm.sh
+++ config/zsh/plugins/nvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NVM_DIR = "${XDG_DATA_HOME}/nvm/"
+export NVM_DIR = "${XDG_DATA_HOME}/nvm"
 if [[ ! -d "$NVM_DIR" ]]; then
     mkdir -p "$NVM_DIR"
     git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
