0300c76cce3688331630eb92613b04a88a0a32de
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-22 14:28:22 +0530

    refactor(zsh): replaces paths with env in docker-containers

--- config/zsh/plugins/docker-containers.sh
+++ config/zsh/plugins/docker-containers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-alias cstart="$HOME/.local/lib/docker-containers/start.sh"
-if ! which docker 2>/dev/null; then
+alias cstart="${LIB_PATH}/docker-containers/start.sh"
+if ! command -v docker 2>/dev/null; then
     alias docker="podman"
 fi
 
-if [ -d "$HOME/.local/lib/docker-containers" ]; then
+if [ -d "${LIB_PATH}/docker-containers" ]; then
     exit 0
 fi
 
-git clone https://github.com/kareem-abdul/docker-containers.git ~/.local/lib/docker-containers
+git clone https://github.com/kareem-abdul/docker-containers.git ${LIB_PATH}/docker-containers
 
