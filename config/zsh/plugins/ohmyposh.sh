20770c057c200a7486b7010022c43b2d1f6c4ea2
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-22 14:12:43 +0530

    feat(zsh): adds custom ohmyposh cli plugin

---
+++ config/zsh/plugins/ohmyposh.sh
@@ -0,0 +1,13 @@
+OMP_HOME="${XDG_DATA_HOME}/.local/share}/ohmyposh"
+
+if [ ! -d "$OMP_HOME" ]; then
+    mkdir -p $OMP_HOME/{bin,themes}
+    executable=$OMP_HOME/bin/oh-my-posh
+    curl -s -f -L https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -o $executable
+    curl -s -f -L https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -o $OMP_HOME/themes.zip
+    chmod +x $executable
+    unzip -o -q $OMP_HOME/themes.zip -d $OMP_HOME/themes
+    rm $OMP_HOME/themes.zip
+fi
+
+eval "$($OMP_HOME/bin/oh-my-posh init zsh)"
