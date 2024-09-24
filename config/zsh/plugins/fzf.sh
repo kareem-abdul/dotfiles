81b0db33804129886cecd585639758b73f0411d2
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-09-24 17:19:25 +0530

    feat(zsh): moves fzf binary to BIN_PATH when installed through zinit

--- config/zsh/plugins/fzf.sh
+++ config/zsh/plugins/fzf.sh
@@ -1,6 +1,5 @@
 if ! command -v fzf &> /dev/null; then
-    echo "fzf does not exists. Installing from github releases"
-    zi ice from"gh-r" as"program"
+    zi ice from"gh-r" as"program" cp"fzf -> $BIN_PATH"
     zi light junegunn/fzf
 fi
 eval "$(fzf --zsh)"
