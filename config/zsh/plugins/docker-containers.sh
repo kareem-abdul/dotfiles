47f7c96c228ddb9cd257e937bbf98b92a818a9cd
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-10-03 23:38:14 +0530

    feat(zsh): updates docker-container plugint to zinit

--- config/zsh/plugins/docker-containers.sh
+++ config/zsh/plugins/docker-containers.sh
@@ -1,11 +1,10 @@
+if ! command -v dc_start &>/dev/null; then
+    zi ice as"program" cp"dc_start -> $BIN_PATH"
+    zi light kareem-abdul/docker-containers
+fi
+
+export DOCKER_CONTAINER_HOME="$(dirname $ZINIT_HOME)/plugins/kareem-abdul---docker-containers"
+
 if ! command -v docker &>/dev/null; then
     alias docker="podman"
 fi
-
-if [ -d "${LIB_HOME}/docker-containers" ]; then
-    return 0
-fi
-
-git clone https://github.com/kareem-abdul/docker-containers.git ${LIB_HOME}/docker-containers
-ln -s ${LIB_HOME}/docker-containers/dc_start $BIN_PATH/dc_start
-
