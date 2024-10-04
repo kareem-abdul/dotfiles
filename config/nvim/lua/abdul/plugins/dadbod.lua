e1389b7a8b31ce88cc911546a621c4af1fe48e47
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-10-04 21:17:43 +0530

    feat(nvim): adds vim-dadbod-completion

--- config/nvim/lua/abdul/plugins/dadbod.lua
+++ config/nvim/lua/abdul/plugins/dadbod.lua
@@ -3,7 +3,8 @@ return {
     'kristijanhusak/vim-dadbod-ui',
     lazy = true,
     dependencies = {
-        { 'tpope/vim-dadbod', cmd = { 'DB' } }
+        { 'tpope/vim-dadbod', cmd = { 'DB' } },
+        { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }
     },
     cmd = {
         'DBUI',
@@ -11,4 +12,7 @@ return {
         'DBUIAddConnection',
         'DBUIFindBuffer',
     },
+    init = function()
+        vim.g.db_ui_use_nerd_fonts = 1
+    end
 };
