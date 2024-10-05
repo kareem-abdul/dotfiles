578aa9f39ca9726a495ceac3fe5a83ac09a0a5f2
kareem-abdul <67004443+kareem-abdul@users.noreply.github.com>
2024-10-05 13:30:15 +0530

    fix(nvim): resolves invalid autocmd for lsp_signature

--- config/nvim/lua/abdul/plugins/lsp_signature.lua
+++ config/nvim/lua/abdul/plugins/lsp_signature.lua
@@ -1,14 +1,17 @@
 return {
     "ray-x/lsp_signature.nvim",
     lazy = true,
-    init = function(options, args)
+    init = function()
         vim.api.nvim_create_autocmd("LspAttach", {
-            require('lsp_signature').on_attach({
-                bind = true,
-                handler_opts = {
-                    border = "rounded"
-                }
-            })
+            group = vim.api.nvim_create_augroup("lsp_signature_LSPATTACH", { clear = true }),
+            callback = function()
+                require('lsp_signature').on_attach({
+                    bind = true,
+                    handler_opts = {
+                        border = "rounded"
+                    }
+                })
+            end
         })
     end
 }
